eb8b8f99ddee26976ea8c9ab429ea7c9e4dab081
Bas <bvanduerendenhollander@gmail.com>
2018-07-22 14:25:52 +0200

    Train on sin(x) instead of XOR

--- main.js
+++ main.js
@@ -1,34 +1,26 @@
 
-var trainingData = [
-    {
-        inputs: [0, 0], 
-        outputs: [0]
-    },
-    {
-        inputs: [0, 1], 
-        outputs: [1]
-    },
-    {
-        inputs: [1, 0], 
-        outputs: [1]
-    },
-    {
-        inputs: [1, 1], 
-        outputs: [0]
-    }
-]
+var trainingData = []
+
+var dataSize = 50;
+for (var i = 0; i < dataSize; i++) {
+    var input = (1 / dataSize) * i;
+    var output = (Math.sin(input * 2 * Math.PI) + 1) / 2;
+    trainingData.push({
+        inputs: [input],
+        outputs: [output]
+    });
+}
 
 var n = new Network(trainingData, 0.5);
 
 n.addInput(0);
-n.addInput(0);
-for (var i = 1; i < 3; i++) {
-    for (var j = 0; j < 2; j++) {
+for (var i = 1; i < 5; i++) {
+    for (var j = 0; j < 5; j++) {
         var neuron = new Neuron(NeuronTypes.TANH);
         n.addNeuron(neuron, i);
     }
 }
 var neuron = new Neuron(NeuronTypes.TANH);
-n.addNeuron(neuron, 3);
+n.addNeuron(neuron, 5);
 
 n.train();
